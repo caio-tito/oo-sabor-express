1c013ca409e9d4dc381b38df445161e27d749bd2
Caio Tito <caiotito96@gmail.com>
2026-07-18 21:02:50 -0300

    Update restaurante.py

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -3,8 +3,13 @@ class Restaurante:
     categoria = ''
     ativo = False
 
-    restaurante_praca = Restaurante()
-    restaurante_praca.nome = 'Praça'
 
-    restaurante_pizza = Restaurante()
-    restaurante_pizza.nome = 'Pizza'
+restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Gourmet'
+
+restaurante_pizza = Restaurante()
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(vars(restaurante_praca))
