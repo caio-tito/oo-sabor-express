650e99982300d2dc111dedf5769cd72444f3567a
Caio Tito <caiotito96@gmail.com>
2026-07-29 21:43:21 -0300

    Criando a classe de avaliação

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicano')
 restaurante_japones = Restaurante('Japa', 'Japonesa')
 
+restaurante_praca.receber_avaliacao('João', 5)
+restaurante_praca.receber_avaliacao('Maria', 4)
+restaurante_praca.receber_avaliacao('Pedro', 3)
+
+restaurante_mexicano.alternar_estado()
+
 def main():
     Restaurante.listar_restaurantes()
 
